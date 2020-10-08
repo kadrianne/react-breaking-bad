72920aedfaae5774006e586ff25af1dae0211944
Kristine Du <kristine.a.du@gmail.com>
2020-10-08 13:19:57 -0600

    Build out app structure with styling.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    characters: [],
+    alive: [],
+    deceased: []
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Breaking Bad</h1>
+        <main>
+          <section className='all-characters'>
+            <h2>All Characters</h2>
+          </section>
+          <div className='sorted-characters'>
+            <section className='alive'>
+              <h2>Alive</h2>
+            </section>
+            <section className='deceased'>
+              <h2>Deceased</h2>
+            </section>
+        </div>
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card(){
+
+  return (
+    <div className='card'>
+      <h3>Character's Name</h3>
+      <button>Alive</button>
+      <button>Deceased</button>
+    </div>
+  )
+}
